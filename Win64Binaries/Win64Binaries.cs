bd3c9c887ed66d3e3aca3787da3a860d52e914d6
JavaCommons Technologies <javacommons@gmail.com>
2026-02-07 10:06:20 +0900

    Win64Binaries-v2026.0207.1006.14

--- Win64Binaries/Win64Binaries.cs
+++ Win64Binaries/Win64Binaries.cs
@@ -13,11 +13,10 @@ public static class Win64Binaries
             Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),
             ".ProgramData",
             "Win64Binaries");
-        string instDir = Installer.InstallResourceZip(
-            typeof(Win64Binaries).Assembly,
-            instRoot,
-            "Win64Binaries:Programs.resource");
-        instDir = instDir.Replace("\\", "/");
+        string instDir = Installer.InstallZipFromURL(
+            "https://github.com/nuget-pkg/Win64Binaries/releases/download/Win64Binaries.v2026.0207.0956.36/Win64Binaries.zip",
+           instRoot,
+            "Win64Binaries");
         return instDir;
     }
 }
